a6207393daa0c497309cec67264d90d2da8349d5
hasalina <shahhassan818@gmail.com>
2019-12-25 22:23:35 +0900

    Added Shope Page

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import './pages/homepage/homepage.styles.scss';
 
-const HatsPage = () => (
+/*const HatsPage = () => (
 	<div>
 		<h1>HATS PAGE</h1>
-	</div>
-)
+	</div>)*/
 
 function App() {
   return (   /*is true if not specified*/
     <div>   {/*should be a string. represents base path denoted by /*/} 
       <Switch>
       <Route exact path='/' component = {HomePage}/>      
-      <Route path='/hats' component = {HatsPage}/>
+      <Route path='/shop' component = {ShopPage}/>
       </Switch>     
      {/* <HomePage/>*/}      
     </div>
